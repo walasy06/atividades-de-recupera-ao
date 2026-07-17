3e2e3100453e9bcde619bb0dd8b15259a5940419
anaoTRICOLOR <walasyhyan@gmail.com>
2026-07-17 00:40:07 -0300

    Improve input prompts for weight and height
    Updated input prompts for weight and height.

--- att2.py
+++ att2.py
@@ -13,8 +13,8 @@ else:
 
 2) melhorar imc
 
-peso = float(input("Digite seu peso em kg (ex: 70.5): "))
-altura = float(input("Digite sua altura em metros (ex: 1.75): "))
+peso = float(input("Digite seu peso em kg : "))
+altura = float(input("Digite sua altura em metros : "))
 imc = peso / (altura ** 2)
 
 f imc < 18.5:
