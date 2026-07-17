c14fc07a5332448b096382b6df0e34a86878a714
anaoTRICOLOR <walasyhyan@gmail.com>
2026-07-17 00:12:21 -0300

    Add rectangle area calculation in areatriangulo.py

---
+++ areatriangulo.py
@@ -0,0 +1,4 @@
+base = float(input("Digite o valor da base do retângulo: "))
+altura = float(input("Digite o valor da altura do retângulo: "))
+area = base * altura
+print(f"A área do retângulo é: {area}")
